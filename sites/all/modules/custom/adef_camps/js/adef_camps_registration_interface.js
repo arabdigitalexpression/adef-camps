f1c17a77781a200931d0e8323f34cbfaf9d92158
Shady Samir <shadysamirsobhi@gmail.com>
2015-07-26 05:16:50 +0200

    Resolve timeslot conflicts with non-uniform time table

--- sites/all/modules/custom/adef_camps/js/adef_camps_registration_interface.js
+++ sites/all/modules/custom/adef_camps/js/adef_camps_registration_interface.js
@@ -1,12 +1,12 @@
-(function($) {
+(function ($) {
   var timer;
   timer = setInterval(getSlots, 10000);
   getSlots();
-  $().ready(function() {
+  $().ready(function () {
     //prepare sessions interaction
     Drupal.adefCamps.formatSchedule('#available-sessions .view-content', 2, adefCampsDaysToShow, adefCampsStartDate);
     $('.tooltip').hide();
-    $('.session').mouseover(function() {
+    $('.session').mouseover(function () {
       if (getClassByPrefix($(this), 'id-') == 'id-') {
         return;
       }
@@ -18,13 +18,13 @@
       if (!$(this).hasClass('disabled')) {
         $(this).children('.tooltip').show().children().show();
       }
-    }).mouseleave(function() {
+    }).mouseleave(function () {
       $('.' + getClassByPrefix($(this), 'id-')).removeClass('repeated');
       if ((!$(this).hasClass('selected')) && $('.' + getClassByPrefix($(this), 'w-') + '.selected').length == 0) {
         $('.' + getClassByPrefix($(this), 'w-')).removeClass('required');
       }
       $(this).children('.tooltip').hide();
-    }).click(function() {
+    }).click(function () {
       if (getClassByPrefix($(this), 'id-') == 'id-') {
         return;
       }
@@ -33,13 +33,15 @@
       }
       if ($(this).hasClass('selected')) {
         $(this).removeClass('selected');
-        $('.' + getClassByPrefix($(this), 'tc-')).removeClass('disabled');
+        timeOverlap(this, false);
+        //$('.' + getClassByPrefix($(this), 'tc-')).removeClass('disabled');
         if ($('.' + getClassByPrefix($(this), 'w-') + '.selected').not('.' + getClassByPrefix($(this), 'id-')).length > 0
                 && $('.' + getClassByPrefix($(this), 'id-') + '.selected').length == 0) {
           $('.' + getClassByPrefix($(this), 'id-')).addClass('required');
         }
       } else {
-        $('.' + getClassByPrefix($(this), 'tc-')).addClass('disabled');
+        timeOverlap(this, true);
+        //$('.' + getClassByPrefix($(this), 'tc-')).addClass('disabled');
         $(this).addClass('selected');
         $('.' + getClassByPrefix($(this), 'id-')).removeClass('required');
       }
@@ -72,11 +74,28 @@
     return '';
   }
 
+  function timeOverlap(session, disable) {
+    $('.session[data-daycode = "' + $(session).data('daycode') + '"]').each(function (pos, testSession) {
+      sessionStart = $(session).data('startts');
+      sessionEnd = $(session).data('endts');
+      testSessionStart = $(testSession).data('startts');
+      testSessionEnd = $(testSession).data('endts');
+      if ((sessionStart < testSessionEnd) && (sessionEnd > testSessionStart)) {
+        if (disable) {
+          $(testSession).addClass('disabled');
+        } else {
+          $(testSession).removeClass('disabled');
+        }
+      }
+    });
+  }
+
   function updateSelected() {
     var selected = new Array();
-    $('.session.selected').each(function(pos, session) {
+    $('.session.selected').each(function (pos, session) {
       cssclasses = $(session).prop('class').split(' ');
       for (i = 0; i < cssclasses.length; i++) {
+        zkid = '';
         if (cssclasses[i].substr(0, 11) == 'zkid') {
           zkid = cssclasses[i].replace('zkid-', '');
         }
@@ -88,8 +107,8 @@
 
   function getSlots() {
     var url = '/?q=adefcamps/registration/slots';
-    $.getJSON(url, function(json) {
-      $.each(json, function(key, val) {
+    $.getJSON(url, function (json) {
+      $.each(json, function (key, val) {
         if (val > 0) {
           message = Drupal.t("Remaining slots: @slots", {"@slots": val});
         } else {
--- sites/all/modules/custom/adef_camps/templates/views-view-unformatted--content-blocks--page-schedule.tpl.php
+++ sites/all/modules/custom/adef_camps/templates/views-view-unformatted--content-blocks--page-schedule.tpl.php
@@ -13,11 +13,13 @@
 <?php foreach ($rows as $id => $row): ?>
   <?php
   $daycode = $view->result[$id]->field_field_date_time[0]['raw']['value'];
-  $start = date('Hi', strtotime($daycode));
-  $daycode = date('Y-m-d', strtotime($daycode));
+  $start_timestamp = strtotime($daycode);
+  $start = date('Hi', $start_timestamp);
+  $daycode = date('Y-m-d', $start_timestamp);
   $duration = isset($view->result[$id]->field_field_duration[0]) ? $view->result[$id]->field_field_duration[0]['raw']['value']: "120";
+  $end_timestamp = $start_timestamp + ($duration * 60)
   ?>
-  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  }?> data-daycode="<?php print $daycode; ?>" data-start="<?php print $start; ?>" data-duration="<?php print $duration; ?>">
+  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  }?> data-daycode="<?php print $daycode; ?>" data-start="<?php print $start; ?>" data-duration="<?php print $duration; ?>" data-startts="<?php print $start_timestamp; ?>" data-endts="<?php print $end_timestamp; ?>">
     <?php print $row; ?>
   </div>
 <?php endforeach; ?>
