d548487781b720472e7a4c6cf31b119f3faf72ed
Shady Samir <shadysamirsobhi@gmail.com>
2015-08-16 08:21:51 +0200

    Fix participant display when profile photo is not present

--- sites/all/themes/camps/js/script.js
+++ sites/all/themes/camps/js/script.js
@@ -1,9 +1,13 @@
-(function($) {
-  $().ready(function() {
-    $('.profile-header-image img').mouseover(function() {
-      $('#participant-profile-home .center-wrapper').css({'margin-top':'0','background-color': 'rgba(255,255,255,0)'});
-    }).mouseout(function() {
-      $('#participant-profile-home .center-wrapper').css({'margin-top':'','background-color': ''});
-    });
+(function ($) {
+  $().ready(function () {
+    if ($('.profile-header-image img').length) {
+      $('.profile-header-image img').mouseover(function () {
+        $('#participant-profile-home .center-wrapper').css({'margin-top': '0', 'background-color': 'rgba(255,255,255,0)'});
+      }).mouseout(function () {
+        $('#participant-profile-home .center-wrapper').css({'margin-top': '', 'background-color': ''});
+      });
+    } else {
+      $('#participant-profile-home .center-wrapper').css({'margin-top': '0', 'background-color': 'rgba(255,255,255,0)'});
+    }
   });
 }(jQuery));
